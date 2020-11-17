8a74890c17aa82de6e20e5f125ba7ec3c5735e50
dong-woogie <com6511@gmail.com>
2020-11-17 16:22:21 +0900

    update resolver, dto, service 코드추가

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,5 +1,6 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantSerivce } from './restaurants..service';
 
@@ -18,6 +19,19 @@ export class RestaurantResolver {
   ): Promise<boolean> {
     try {
       await this.restaurantSerivce.createRestaurant(createRestaurantDto);
+      return true;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  }
+
+  @Mutation(() => Boolean)
+  async updateRestaurant(
+    @Args() updateRestaurantDto: UpdateRestaurantDto,
+  ): Promise<boolean> {
+    try {
+      await this.restaurantSerivce.updateRestaurant(updateRestaurantDto);
       return true;
     } catch (e) {
       console.log(e);
