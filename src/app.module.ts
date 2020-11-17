2b9b1f0cd321b658c73a154b6ef9a787eb4f3bbc
dong-woogie <com6511@gmail.com>
2020-11-17 18:54:35 +0900

    start clone coding ( part 1 User CRUD )

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import * as Joi from 'joi';
 import { Restaurant } from './restaurants/entities/restaurant.entity';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { UsersModule } from './users/users.module';
+import { CommonModule } from './common/common.module';
+import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [
@@ -30,12 +33,15 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       database: process.env.DB_NAME,
       synchronize: true,
       logging: true,
-      entities: [Restaurant],
+      entities: [User],
+      // Restaurant,
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
-    RestaurantsModule,
+    // RestaurantsModule,
+    UsersModule,
+    CommonModule,
   ],
   controllers: [],
   providers: [],
