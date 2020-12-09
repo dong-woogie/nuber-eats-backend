76940ff81d4d3073e05d92497bc81298aa944061
dong-woogie <com6511@gmail.com>
2020-12-09 20:22:25 +0900

    add Mail Module, Mail Entity, Mail Interfaces, Mail Dto

---
+++ src/users/entities/verification.entity.ts
@@ -0,0 +1,22 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { CoreEntity } from 'src/common/entities/core.entity';
+import { BeforeInsert, Column, Entity, JoinColumn, OneToOne } from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+import { User } from './user.entity';
+
+@Entity()
+@InputType({ isAbstract: true })
+export class Verification extends CoreEntity {
+  @Column()
+  @Field(type => String)
+  code: string;
+
+  @OneToOne(type => User, { onDelete: 'CASCADE' })
+  @JoinColumn()
+  user: User;
+
+  @BeforeInsert()
+  createCode(): void {
+    this.code = uuidv4().replace(/-/gi, '');
+  }
+}
