038ee9e9cfeed9d6e712628d457049c4fde16aea
dong-woogie <com6511@gmail.com>
2021-04-29 02:36:03 +0900

    옵션변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -70,7 +70,7 @@ import { UploadsModule } from './uploads/uploads.module';
       ],
     }),
     GraphQLModule.forRoot({
-      playground: true,
+      playground: process.env.NODE_ENV !== 'production',
       installSubscriptionHandlers: true,
       autoSchemaFile: true,
       context: ({ req, connection }) => {
